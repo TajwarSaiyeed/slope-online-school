a2a000761a3e7d3099e8d416c0a1ed2a48435926
TajwarSaiyeed <rjabid36@gmail.com>
2023-09-29 22:15:38 +0600

    bug fix

--- app/(dashboard)/(routes)/teacher/courses/[courseId]/chapters/[chapterId]/_components/chapter-access-form.tsx
+++ app/(dashboard)/(routes)/teacher/courses/[courseId]/chapters/[chapterId]/_components/chapter-access-form.tsx
@@ -88,7 +88,6 @@ export const ChapterAccessForm = ({initialData, courseId, chapterId}: ChapterAcc
                         <FormItem className={'flex flex-row items-start space-x-3 space-y-0 rounded-md border p-4'}>
                             <FormControl>
                                 <Checkbox
-                                    {...field}
                                     checked={field.value}
                                     onCheckedChange={field.onChange}
                                 />
